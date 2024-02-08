c2e8553b3999ef6a4d0b4788f53fa02d3fdc09c1
Jesse Strangfeld <jesse.strangfeld@gmail.com>
2024-02-08 12:40:29 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -93,7 +93,7 @@ async fn post_webhook(
         ("dvrpc/rtsp-api", "rtsp"),
         ("dvrpc/tp-updates", "tp_updates"),
         ("dvrpc/cjtf", "cjtf"),
-        ("dvrpc/regional_housing", "housing_submarkets"),
+        ("dvrpc/regional-housing", "housing_submarkets"),
     ]);
 
     // Get path and log from context.
